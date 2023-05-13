09cf1bd2d143405c917979b8eacd6f095a22c602
fendishatech <liyukibir@gmail.com>
2023-05-13 09:01:26 -0700

    home page basic design

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Home</h1>
-    </div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur,
+        mollitia odit animi quas consequatur eius. Cumque quibusdam numquam quod
+        non dignissimos alias recusandae, nam ex aut officia reiciendis laborum
+        ipsa debitis vitae quis quasi quisquam voluptate, harum a quam omnis?
+      </p>
+      {/* Feed Component */}
+    </section>
   );
 };
 
